9dab134eacc53b71278f55492c306b813adddf28
vgabriel04 <victor_gabriels04@hotmail.com>
2022-02-19 12:00:41 -0400

    add dark mode

--- script.js
+++ script.js
@@ -18,22 +18,22 @@ const isNumber = (numero) => /^[0-9]+$/.test(numero);
 const cepValido = (cep) => cep.length == 8 && isNumber(cep);
 
 const pesquisarCep = async () => {
-  const cep = document.getElementById("cep").value;
-  const url = `http://viacep.com.br/ws/${cep}/json/`;
+  const cep = document.getElementById("cep").value; // 1
+  const url = `http://viacep.com.br/ws/${cep}/json/`; // 2
   // fetch(url).then(response => response.json()).then(console.log)
 
   if (cepValido(cep)) {
     limparForm();
 
-    const dados = await fetch(url);
-    const endereco = await dados.json();
+    const dados = await fetch(url); // 3
+    const endereco = await dados.json(); // 4
     if (endereco.hasOwnProperty("erro")) {
       document.getElementById("endereco").value = "CEP não encontrado!";
       document.getElementById("bairro").value = "";
       document.getElementById("cidade").value = "";
       document.getElementById("estado").value = "";
     } else {
-      preencherForm(endereco);
+      preencherForm(endereco); //5
     }
   } else {
     document.getElementById("endereco").value = "CEP incorreto!";
@@ -41,3 +41,10 @@ const pesquisarCep = async () => {
 };
 
 document.getElementById("cep").addEventListener("focusout", pesquisarCep);
+
+//----------------- BTN DARK MODE ----------------
+
+function darkMode() {
+  const element = document.body;
+  element.classList.toggle("darkmode");
+}
